f4b4c1f36ce46abd4d72aee355ed8d63732485c0
Bryan Hou <boyuhou@gmail.com>
2018-05-30 22:18:28 -0400

    latest update

--- dataservice/listeners.py
+++ dataservice/listeners.py
@@ -44,11 +44,30 @@ class QuoteListener(VerboseIQFeedListener):
 
     def process_live_bar(self, bar_data: np.array) -> None:
         print("%s: Process live bar:" % self._name)
-        print(bar_data)
+        min_interval = int(int(bar_data['id'][0].split('-')[2]) / 60)
+        ticker = bar_data['symbol'][1]
 
     def process_history_bar(self, bar_data: np.array) -> None:
         print("%s: Process history bar:" % self._name)
         print(bar_data)
+        min_interval = int(int(bar_data['id'][0].split('-')[2]) / 60)
+        ticker = bar_data['symbol'][1]
+        close_price = bar_data['close_p'][0]
+        quote_time = bar_data['datetime'][0]
+        if min_interval == 5:
+            self.data_dict[ticker].min_05_list.append(close_price)
+            self.data_dict[ticker].datetime_05_list.append(close_price)
+        elif min_interval == 15:
+            self.data_dict[ticker].min_15_list.append(close_price)
+            self.data_dict[ticker].datetime_15_list.append(close_price)
+        elif min_interval == 60:
+            self.data_dict[ticker].min_60_list.append(close_price)
+            self.data_dict[ticker].datetime_60_list.append(close_price)
+        elif min_interval == 240:
+            self.data_dict[ticker].min_240_list.append(close_price)
+            self.data_dict[ticker].datetime_240_list.append(close_price)
+        else:
+            raise KeyError('not supported mins')
 
     def process_invalid_symbol(self, bad_symbol: str) -> None:
         print("%s: Invalid Symbol: %s" % (self._name, bad_symbol))
